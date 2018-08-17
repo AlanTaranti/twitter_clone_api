8fab98ed81a2acc38ecef5d370713428589a71c6
Alan Taranti <alan.taranti@gmail.com>
2018-08-17 13:02:54 -0400

    adicionado url de comentario

--- TwitterClone/urls.py
+++ TwitterClone/urls.py
@@ -6,6 +6,7 @@ from TwitterClone.views import *
 router = DefaultRouter()
 router.register('usuarios', PerfilViewSet, base_name='usuarios')
 router.register('tweets', TweetViewSet, base_name='tweets')
+router.register('comentarios', ComentarioViewSet, base_name='comentarios')
 
 #
 # Url Patterns
