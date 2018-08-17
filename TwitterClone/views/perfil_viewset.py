615969be1cf9efa5b6f534eb4ecfe0690562b1b2
Alan Taranti <alan.taranti@gmail.com>
2018-08-17 15:19:17 -0400

    adicionado ações de seguir e desseguir

--- TwitterClone/views/perfil_viewset.py
+++ TwitterClone/views/perfil_viewset.py
@@ -1,6 +1,9 @@
 from TwitterClone.serializers import PerfilSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from django.contrib.auth.models import User
+from TwitterClone.models import Perfil
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class PerfilViewSet(viewsets.ModelViewSet):
@@ -8,3 +11,23 @@ class PerfilViewSet(viewsets.ModelViewSet):
     serializer_class = PerfilSerializer
     filter_fields = ('username', 'email')
     search_fields = ('username', 'first_name', 'last_name', 'email')
+
+    @action(methods=['post'], detail=True)
+    def seguir(self, request, pk=None):
+        perfil_a_seguir = User.objects.get(pk=pk).perfil
+        Perfil.objects.get(usuario=request.user).segue.add(perfil_a_seguir)
+
+        serializer = PerfilSerializer(data=perfil_a_seguir.usuario)
+        serializer.is_valid()
+
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=True)
+    def desseguir(self, request, pk=None):
+        perfil_a_desseguir = User.objects.get(pk=pk).perfil
+        Perfil.objects.get(usuario=request.user).segue.remove(perfil_a_desseguir)
+
+        serializer = PerfilSerializer(data=perfil_a_desseguir.usuario)
+        serializer.is_valid()
+
+        return Response(serializer.data)
