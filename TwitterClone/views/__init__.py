1f05932defce0448fc93c47b05b3867bd809d139
Alan Taranti <alan.taranti@gmail.com>
2018-08-17 12:30:54 -0400

    adicionado viewset de tweet

--- TwitterClone/views/__init__.py
+++ TwitterClone/views/__init__.py
@@ -1,1 +1,2 @@
 from . perfil_viewset import PerfilViewSet
+from . tweet_viewset import TweetViewSet
---
+++ TwitterClone/views/tweet_viewset.py
@@ -0,0 +1,25 @@
+import django_filters
+from TwitterClone.serializers import TweetSerializer
+from rest_framework import viewsets
+from TwitterClone.models import Tweet
+
+
+class TweetFilter(django_filters.FilterSet):
+    usuario = django_filters.CharFilter(lookup_expr='username__exact')
+
+    class Meta:
+        model = Tweet
+        fields = ('usuario',)
+
+
+class TweetViewSet(viewsets.ModelViewSet):
+    queryset = Tweet.objects.all()
+    serializer_class = TweetSerializer
+    filter_class = TweetFilter
+    search_fields = ('texto',)
+
+    def get_queryset(self):
+        queryset = Tweet.objects.all()
+        # Configurar "carregamento ansioso" (o invés do lazy loading do Django) para evitar selects N+1
+        queryset = self.get_serializer_class().setup_eager_loading(queryset)
+        return queryset
