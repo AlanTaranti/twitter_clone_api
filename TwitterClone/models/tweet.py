3bab050c1e7f67806e8b46ef59f3df39b4f632a1
Alan Taranti <alan.taranti@gmail.com>
2018-08-17 13:01:46 -0400

    adicionado related name para usuarios em tweet

--- TwitterClone/models/tweet.py
+++ TwitterClone/models/tweet.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Tweet(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, related_name='tweets', on_delete=models.CASCADE)
 
     # O limite do Tweet original é 280. Vamos ser fiéis, :-P
     texto = models.CharField(max_length=280)
