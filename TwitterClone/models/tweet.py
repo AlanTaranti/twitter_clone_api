5cca65fe037a752accfa2e8eddd88bfd7c72bb6e
Alan Taranti <alan.taranti@gmail.com>
2018-08-17 12:19:22 -0400

    alterado chave estrangeira de Perfil para User em tweet

--- TwitterClone/models/tweet.py
+++ TwitterClone/models/tweet.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Tweet(models.Model):
-
-    usuario = models.ForeignKey('Perfil', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # O limite do Tweet original é 280. Vamos ser fiéis, :-P
     texto = models.CharField(max_length=280)
